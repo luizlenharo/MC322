8b03976c8844a3996e1ffa781babdb37aae64ea6
Luiz Felipe Lenharo <l237896@dac.unicamp.br>
2024-03-24 19:29:38 -0300

    lab finalizado

--- Monopoly/Jogador.java
+++ Monopoly/Jogador.java
@@ -1,3 +1,6 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Jogador {
     private String nome;
     private String cpf;
@@ -47,8 +50,65 @@ public class Jogador {
         return "Nome: " + nome + "Cpf: " + cpf + "email: " + email + "foto: " + foto;
     }
 
-    public void validarCPF(String cpf){
+    public boolean validarCPF(String cpf){
         String formatCPF = cpf.replaceAll("[^0-9]+", "");
-        System.out.println(formatCPF);
+
+        //VERIFICANDO SE TEM 11 DÍGITOS
+        if (formatCPF.length() != 11){
+            System.out.printf("CPF inválido");
+            return false;
+        }
+        //VERIFICANDO SE TODOS OS DÍGITOS SÃO IGUAIS
+        int j, iguais;
+        iguais = 0;
+        for (j=1; j<11; ++j){
+            if (formatCPF.charAt(j) != formatCPF.charAt(j-1)){
+                break;
+            } else {
+                ++ iguais;
+            }
+        }
+        if (iguais == 10){
+            System.out.printf("CPF inválido");
+            return false;
+        }
+        //VERIFICAR OS DÍGITOS VERIFICADORES
+        int somatorio = 0;
+        int i;
+        for (i=0; i<9; ++i){
+            somatorio = somatorio + (Character.getNumericValue(formatCPF.charAt(i))*(i+1));
+        }
+        int resto = somatorio%11;
+        if (resto == 10) {
+            resto = 0;
+        }
+        if (resto != Character.getNumericValue(formatCPF.charAt(9))){
+            System.out.printf("CPF inválido");
+            return false;
+        } else {
+            somatorio = 0;
+            for (i=0; i<10; ++i){
+                somatorio = somatorio + (Character.getNumericValue(formatCPF.charAt(i))*i);
+            }
+            resto = somatorio%11;
+            if (resto == 10){
+                resto = 0;
+            }
+            if (resto != Character.getNumericValue(formatCPF.charAt(10))){
+                System.out.printf("CPF inválido");
+                return false;
+            }
+        }
+
+        return true;
     }
+
+    public boolean validarEmail(String email) {
+        String EMAIL_REGEX = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+
+        Pattern pattern = Pattern.compile(EMAIL_REGEX);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
 }
--- Monopoly/Main.java
+++ Monopoly/Main.java
@@ -2,28 +2,30 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-       System.out.println("ola mundo");
        //LER JOGADOR
        String nome, cpf, foto, email;
        Scanner entrada = new Scanner(System.in);
 
        System.out.print("informe o nome: ");
        nome = entrada.nextLine();
-       System.out.printf("Escreveu: %s\n", nome);
 
        System.out.print("informe o cpf: ");
        cpf = entrada.nextLine();
-       System.out.printf("Escreveu: %s\n", cpf);
 
        System.out.print("informe o foto: ");
        foto = entrada.nextLine();
-       System.out.printf("Escreveu: %s\n", foto);
 
        System.out.print("informe o email: ");
        email = entrada.nextLine();
-       System.out.printf("Escreveu: %s\n", email);
 
        Jogador p1 = new Jogador(nome, cpf, email, foto);
-       p1.validarCPF(cpf);
+
+       if (p1.validarCPF(p1.getCpf())){
+          System.out.printf("CPF válido!\n");
+       }
+
+       if (p1.validarEmail(p1.getEmail())){
+          System.out.printf("Email válido!\n");
+       }
     }
 }
