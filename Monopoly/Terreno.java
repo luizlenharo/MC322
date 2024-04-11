e900952d014edb22b4c2682635164e133c87c902
Luiz Felipe Lenharo <l237896@dac.unicamp.br>
2024-04-10 21:02:57 -0300

    10/04/2024 - finalizar comprarHotel

--- Monopoly/Terreno.java
+++ Monopoly/Terreno.java
@@ -41,6 +41,22 @@ public class Terreno extends Propriedade {
     }
 
     public boolean comprarCasa() {
+        /*Verifica as condições para obtenção da casa na propriedade que são:
+         - o jogador tem que ter todas os terrenos da mesma cor;
+         - os outros terrenos da mesma cor tem que ter no mínimo o mesmo numero
+           de casas do terreno em que a casa quer ser comprada;
+         - o terreno tem que ter no máximo 3 casas no momento;
+         - o jogador tem que ter dinheiro suficiente para comprar a casa
+        Caso alguma das condições não seja cumprida, a compra falha e a função
+        retorna false. Caso contrário, o numero de casas do terreno aumenta em
+        1, o valor da casa é descontado do novo proprietário e a função retorna
+        true.
+        */
+        if (numeroCasas > 3) {
+            return false;
+        } else {
+            numeroCasas ++;
+        }
         return true;
     }
 
